ed88f94734c51ce29634989fe8d1017959a95560
daivd_abu <david@livly.com>
2024-01-24 21:09:27 +0200

    fix env

--- server/server.js
+++ server/server.js
@@ -1,24 +1,24 @@
-const logger = require('log4js').getLogger('Mailer');
-// postgre should always go first
-const postgre = require('./config/postgre');
-const {sleep} = require('./shared/utils');
-const {sendEmailDBEnqueued} = require('./helpers/mailer');
+require("module-alias/register");
 
+const logger = require("log4js").getLogger("Mailer");
+// postgre should always go first
+const postgre = require("./config/postgre");
+const { sleep } = require("./shared/utils");
+const { sendEmailDBEnqueued } = require("./helpers/mailer");
 
 async function run() {
-  sendEmailDBEnqueued();
+    sendEmailDBEnqueued();
 }
 
 async function runServer() {
-  try {
-    await postgre;
-    await sleep(5000);
-    await run();
-  } catch (e) {
-    logger.error('There was an error: ', e);
-    process.exit(1);
-  }
+    try {
+        await postgre;
+        await sleep(5000);
+        await run();
+    } catch (e) {
+        logger.error("There was an error: ", e);
+        process.exit(1);
+    }
 }
 
 runServer();
-
